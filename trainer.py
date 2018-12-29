7c3cde232d2bcd24cd29eb6f5b8039ff885ab8e2
Oxel40 <erik.slott-persson@telia.com>
2018-12-29 16:15:48 +0100

    Kan nu träna nätverk men inte spara

--- trainer.py
+++ trainer.py
@@ -52,8 +52,8 @@ def non_losing_moves(board, lmove):
 	return out
 
 #Define Placeholders
-x = tf.placeholder(tf.float32, shape=[None, 9])
-y_ = tf.placeholder(tf.float32, shape=[None, 9])
+x = tf.placeholder(tf.float32, shape=[None, 9], name = "x")
+y_ = tf.placeholder(tf.float32, shape=[None, 9], name = "y_")
 
 #Define Weights
 W1 = tf.Variable(tf.random_normal(shape=[9, 32]))
@@ -62,17 +62,19 @@ W2 = tf.Variable(tf.random_normal(shape=[32, 9]))
 b1 = tf.Variable(tf.random_normal(shape=[9]))
 
 #Define Graph
-y = tf.nn.sigmoid(tf.matmul(tf.nn.tanh(tf.matmul(x, W1)), W2) + b1)
+y = tf.nn.sigmoid(tf.matmul(tf.nn.tanh(tf.matmul(x, W1)), W2) + b1, name = "y")
 
 ###########
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+loss = tf.losses.softmax_cross_entropy(y_, y)
+optimizer = tf.train.GradientDescentOptimizer(0.1)
+train = optimizer.minimize(loss)
 
 #Initialize a session and variables
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 #Training
-for game in range(1):
+for game in range(100000):
 	board = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.])
 	moves = [[], []]#move[0] is the inputed board layout, move[1] is the outputed move
 	win = False
@@ -102,16 +104,16 @@ for game in range(1):
 					#print(np.reshape(rmove, [3,3]), "r")
 		if check_win(board):
 			win = True
-			print("-"*10, "Turn:", turn, "Game:", game)
-			print(np.reshape(board, [3, 3]) *(-1/(((turn % 2)*2)-1)), "\n")
+			#print("-"*10, "Turn:", turn, "Game:", game)
+			#print(np.reshape(board, [3, 3]) *(-1/(((turn % 2)*2)-1)), "\n")
 			break
-		print("-"*10, "Turn:", turn, "Game:", game)
-		print(np.reshape(board, [3, 3]) *(-1/(((turn % 2)*2)-1)), "\n")
+		#print("-"*10, "Turn:", turn, "Game:", game)
+		#print(np.reshape(board, [3, 3]) *(-1/(((turn % 2)*2)-1)), "\n")
 		board *= -1
 			
 	if win:
-		inputs.append(moves[0][turn % 2 :: 2])#Wining moves
-		lables.append(moves[1][turn % 2 :: 2])#
+		inputs.extend(moves[0][turn % 2 :: 2])#Wining moves
+		lables.extend(moves[1][turn % 2 :: 2])#
 		for index in range((turn + 1) % 2, turn + 1, 2):
 			inputs.append(moves[0][index])
 			lables.append(non_losing_moves(moves[0][index], moves[1][index]))
@@ -119,18 +121,20 @@ for game in range(1):
 		for index in range(0, turn + 1):
 			inputs.append(moves[0][index])
 			lables.append(non_losing_moves(moves[0][index], moves[1][index]))
-	print(inputs)
-	print(lables)
-	#loss = tf.losses.softmax_cross_entropy()
+
+	#print(sess.run(loss,  feed_dict={y_: np.array(lables), x: np.array(inputs)}))
+	sess.run(train, feed_dict={y_: np.array(lables), x: np.array(inputs)})
+	#print(sess.run(loss,  feed_dict={y_: np.array(lables), x: np.array(inputs)}))
+	#print("-"*10)
 
 #Some testing
-#board = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.])
-#test = sess.run(y, feed_dict={x: [board]})
-#ntest = norm_move(test)[0]
+board = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0.])
+test = sess.run(y, feed_dict={x: [board]})
+ntest = norm_move(test)[0]
 #nboard = make_move(board, ntest)
-#print("Board:", board)
-#print("Move:", test)
-#print("Norm move:", ntest)
+print("Board:", board)
+print("Move:", test)
+print("Norm move:", ntest)
 #print("Alowed:", alowed_moves(board))
 #print("Check move:", check_move(board, ntest))
 #print("New board:", nboard)
