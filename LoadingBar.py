f425f92b9f37777383d8f4c49f17a40f8fa83852
Oxel40 <erik.slott-persson@telia.com>
2019-01-02 09:57:01 +0100

    Bigfixes

--- LoadingBar.py
+++ LoadingBar.py
@@ -1,45 +1,43 @@
 class SimpleLoadingBar:
 	def __init__(self):
-		self.prog = 0#Always writen in decimal
+		self.prog = 0.#Floatingpoint
 		self.msg = ""
 		self.maxout = 0
 	
-	#Updates progress and message. If message is not defined then the message will be removed.
-	def Update(self, prog, **kwar):
-		self.msg = ""
+	#Sets progress and message. If message is not defined then the message won't be changed.
+	def Set(self, prog, **kwar):
 		if "msg" in kwar:
-			self.msg =  " : "+kwar["msg"]
-			msg =  " : "+kwar["msg"]
+			self.msg =  kwar["msg"]
+			msg =  kwar["msg"]
 		self.prog = prog
-		out = "\r[{0}{1}]{2}%{3}".format("#"*int(20*self.prog), " "*(20-int(20*self.prog)), self.prog*100, self.msg)
+		out = "\r[{0}{1}]{2}% {3}".format("#"*int(20*self.prog), " "*(20-int(20*self.prog)), self.prog*100, self.msg)
 		if self.maxout > len(out):
 			out += " "*(self.maxout-len(out))
 		else:
 			self.maxout = len(out)
 		print(out, end="")
 		
-	#Makes one step. Does not update message if message is not defined
+	#Makes one step. Does not update message if message is not defined.
 	def Step(self, stepsize = 0.01, **kwar):
 		if "msg" in kwar:
 			self.msg = kwar["msg"]
-		self.Update(self.prog + stepsize)
+		self.Set(self.prog + stepsize)
 		
-	#Start a new loadingbar
+	#Start a new loadingbar.
 	def Start(self, **kwar):
 		self.prog = 0
 		self.maxout = 0
 		self.msg = ""
 		if "msg" in kwar:
-			self.msg = " : "+kwar["msg"]
-		self.Update(self.prog)
+			self.msg = kwar["msg"]
+		self.Set(self.prog)
 		
-	#Sets progress to 100% and updates message. If message is not defined then the message will be removed.
+	#Sets progress to 100% and updates message. If message is not defined then the message won't be changed.
 	def Finnish(self, **kwar):
-		self.msg = ""
 		self.prog = 1
 		if "msg" in kwar:
-			self.msg = " : "+kwar["msg"]
-		out = "\r[{0}]{1}%{2}".format("#"*20, self.prog*100, self.msg)
+			self.msg = kwar["msg"]
+		out = "\r[{0}]{1}% {2}".format("#"*20, self.prog*100, self.msg)
 		if self.maxout > len(out):
 			out += " "*(self.maxout-len(out))
 		else:
@@ -58,7 +56,9 @@ if __name__ == "__main__":
 	time.sleep(1)
 	e.Step(0.005)
 	time.sleep(1)
-	e.Update(0.5, msg = "more testing")
+	e.Set(0.5, msg = "more testing")
+	time.sleep(1)
+	e.Set(0.7533, msg = "")
 	time.sleep(1)
 	e.Finnish(msg = "Done")
 	print("Done")
